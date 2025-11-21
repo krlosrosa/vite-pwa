f0dca62ca8bc2239a2ea3c00a2b813bd9fc084c1
carlos <robertorsilva.krlos@gmail.com>
2025-11-20 22:45:16 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     tailwindcss(), 
     reactRouter(), 
     tsconfigPaths(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true
-      }
-    }),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
 });
