ffb279c783ed753cc53d5dd65ec35260de150559
carlos <robertorsilva.krlos@gmail.com>
2025-11-21 00:06:14 -0300

    first commit

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,6 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -30,7 +29,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/manifest.webmanifest" />
         <Meta />
         <Links />
       </head>
